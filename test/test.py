8631658b00484e246ef8fbd12073550b7361c8bf
WINTOGO-236 <mg_fire1030@163.com>
2026-05-12 09:25:24 +0800

    test

--- test/test.py
+++ test/test.py
@@ -1,44 +1,12 @@
 def test():
         
+    from core.packages.tools import get_hwnd
+    from win32gui import EnumWindows
 
 
-    def get_hwnd_by_process(name="HTGame.exe"):
-        """
-        通过进程名获取窗口句柄
-        返回第一个匹配的窗口句柄，如果没有找到则返回None
-        """
-        import win32gui # type: ignore
-        import win32process # type: ignore
-        from psutil import process_iter
-        for proc in process_iter(['pid']):
-            if proc.name() == name:
-                pid = proc.pid
-                break
-        else:
-            return None
-
-        result = []
-
-        def callback(hwnd, _):
-            if win32gui.IsWindowVisible(hwnd):
-                _, window_pid = win32process.GetWindowThreadProcessId(hwnd)
-                if window_pid == pid:
-                    result.append(hwnd)
-
-        win32gui.EnumWindows(callback, None)
-        return result[0] if result else None
-
-
-    def top_window():
-        from win32gui import SetForegroundWindow # type: ignore
-        hwnd = get_hwnd_by_process()
-        if hwnd:
-            SetForegroundWindow(hwnd)
-    top_window()
-    hwnd = get_hwnd_by_process()
-    if hwnd:
-        print(f"Found window handle: {hwnd}")
-
+    windows = []
+    EnumWindows(get_hwnd, windows)
+    return windows[0] if windows else None
 
     return 0
 
